f1d5ff3768b813e6097d9c62001731ab870e6516
iiHadi66 <44447663+iiHadi66@users.noreply.github.com>
2022-08-24 13:48:38 +0200

    Update str.js

--- str.js
+++ str.js
@@ -8,7 +8,7 @@ client.on('message', message => {
   if (!message.content.startsWith(PrEfix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== '279925567049039883') return;
+  if (message.author.id !== '495890792536211456') return;
 if (message.content.startsWith(PrEfix + 'wt')) {
 client.user.setActivity(argresult, {type:'WATCHING'});
     message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
